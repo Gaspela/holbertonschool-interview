a27aea4df182e40960762c267570f448fc5d617c
Gaspela04 <samirmillanorozco@hotmail.com>
2020-07-21 22:28:19 +0000

    Open boxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+""" method that determines if all the boxes can be opened. """
+
+
+def canUnlockAll(boxes):
+    """Open all boxes"""
+    len_boxes = len(boxes)
+    if not len_boxes:
+        return False
+    unlock = [0]
+
+    for key in unlock:
+        for box in boxes[key]:
+            if box not in unlock and box < len_boxes:
+                unlock.append(box)
+
+    if len(unlock) == len_boxes:
+        return True
+
+    return False
