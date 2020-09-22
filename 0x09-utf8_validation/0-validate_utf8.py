6f07e815cc2703602da1d73b4b0bd447a6afee3b
Gaspela04 <samirmillanorozco@hotmail.com>
2020-09-22 16:28:11 +0000

    utf8 valid

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -11,22 +11,21 @@ def validUTF8(data):
     A character in UTF-8 can be 1 to 4 butes long
     The data set can contain multiple characters
     The data will be represented by a list of integers
-    Each integer represents 1 byte of data, therefore you only need to handle
+    Each integer represents 1 binary of data, therefore you only need to handle
     the 8 least significant bits of each integer
     """
     int_numbytes = 0
     for num in data:
+        binary = format(num, '#010b')[-8:]
         if int_numbytes == 0:
-            if (num >> 5) == 0b110:
-                int_numbytes = 1
-            elif (num >> 4) == 0b1110:
-                int_numbytes = 2
-            elif (num >> 3) == 0b11110:
-                int_numbytes = 3
-            elif (num >> 7):
+            if binary[0] == '1':
+                int_numbytes = len(binary.split('0')[0])
+            if int_numbytes > 4 or int_numbytes == 1:
                 return False
+            if int_numbytes == 0:
+                continue
         else:
-            if (num >> 6) != 0b10:
+            if not (binary[0] == '1' and binary[1] == '0'):
                 return False
-            int_numbytes -= 1
+        int_numbytes -= 1
     return int_numbytes == 0
