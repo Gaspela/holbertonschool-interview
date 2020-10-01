64f5c76c8ad934f5469e46f4df1f9b8e62041872
Gaspela04 <samirmillanorozco@hotmail.com>
2020-10-01 15:30:49 +0000

    Slide line

---
+++ 0x0A-slide_line/slide_line.h
@@ -0,0 +1,12 @@
+#ifndef SLIDE_HLR
+#define SLIDE_HLR
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define SLIDE_LEFT 0
+#define SLIDE_RIGHT 1
+
+int slide_line(int *line, size_t size, int direction);
+
+#endif /* SLIDE_HLR */
