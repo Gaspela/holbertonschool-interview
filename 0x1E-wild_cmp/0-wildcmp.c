80f0a7f4aa1d2843d20965c59cc080597ebf2db4
Samir millan <samirmillanorozco@hotmil.com>
2021-03-30 16:49:52 +0000

    Strings compare

---
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * compare - function to compare strings
+ * @s1: first string
+ * @s2: second string
+ * @ast: flag for asterisk
+ * Return: 1 if the strings can be considered identical, otherwise return 0
+*/
+int compare(char *s1, char *s2, int ast)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s2 == '*')
+		return (compare(s1, s2 + 1, ++ast));
+	else if (*s1 != *s2 && ast > 0)
+	{
+		if (*s1 != '\0')
+			return (compare(s1 + 1, s2, ast));
+		return (0);
+	}
+	else if (*s1 != *s2 && ast == 0)
+		return (0);
+	else if (*s1 == *s2 && ast > 0)
+	{
+		if (compare(s1 + 1, s2 + 1, 0) == 1)
+			return (compare(s1 + 1, s2 + 1, ast));
+		return (compare(s1 + 1, s2, ast));
+	}
+	else if (*s1 == *s2 && ast == 0)
+		return (compare(s1 + 1, s2 + 1, 0));
+	return (-1);
+}
+/**
+* wildcmp - function that compares two strings with recursion
+* @s1: first 1 to compare
+* @s2: second 2 to compare
+* Return: 1 if the strings can be considered identical, otherwise return 0
+*/
+int wildcmp(char *s1, char *s2)
+{
+	int ast = 0;
+
+	return (compare(s1, s2, ast));
+}
