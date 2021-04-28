3357ae3d9269854501eebabeb9758c75a61ffa95
Samir millan <samirmillanorozco@hotmil.com>
2021-04-28 18:31:49 +0000

    Ben win

---
+++ 0x22-primegame/0-prime_game.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Prime Game
+"""
+
+
+def isWinner(x, nums):
+    """
+    where x is the number of rounds and nums is an array of n
+    Return: name of the player that won the most rounds
+    If the winner cannot be determined, return None
+    """
+    if not nums or x < 1:
+        return None
+
+    n = max(nums)
+
+    filter = [True for _ in range(max(n + 1, 2))]
+    
+    for i in range(2, int(pow(n, 0.5)) + 1):
+        if not filter[i]:
+            continue
+        for j in range(i * i, n + 1, i):
+            filter[j] = False
+
+    filter[0] = filter[1] = False
+    c = 0
+    for i in range(len(filter)):
+        if filter[i]:
+            c += 1
+        filter[i] = c
+
+    p1 = 0
+    for n in nums:
+        p1 += filter[n] % 2 == 1
+    if p1 * 2 == len(nums):
+        return None
+    if p1 * 2 > len(nums):
+        return "Maria"
+    return "Ben"
